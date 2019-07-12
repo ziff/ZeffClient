23da256fe560776094da62f8345942d0faa4d165
lanhel <lanhel@flyingtitans.com>
2019-07-11 21:32:10 -0600

    Update tests

--- tests/zeffTestSuite/record/test_unstructureddata.py
+++ tests/zeffTestSuite/record/test_unstructureddata.py
@@ -6,7 +6,7 @@ import enum
 from zeff.record import UnstructuredData, UnstructuredDataItem
 
 
-def test_build():
+def test_valid_http():
     """Test building a UnstructuredData."""
     ud = UnstructuredData()
     assert ud.record is None
@@ -18,6 +18,50 @@ def test_build():
     assert len(list(ud.unstructured_data_items)) == 1
 
     ud.validate()
+    assert udi.accessible == "OK"
+
+
+def test_valid_file():
+    """Test building a UnstructuredData with file."""
+    ud = UnstructuredData()
+    assert ud.record is None
+
+    udi = UnstructuredDataItem(f"file://{__file__}", "text/plain")
+    assert udi.unstructured_data is None
+    udi.unstructured_data = ud
+    assert udi in list(ud.unstructured_data_items)
+    assert len(list(ud.unstructured_data_items)) == 1
+
+    ud.validate()
+    assert udi.accessible == "OK"
+
+
+def test_missing_file():
+    """Test building a UnstructuredData with missing file."""
+    udi = UnstructuredDataItem("file:///spam", "text/plain")
+    udi.validate()
+    assert udi.accessible == "file missing"
+
+
+def test_invalid_file():
+    """Test building a UnstructuredData with invalid file."""
+    udi = UnstructuredDataItem("file:///var", "text/plain")
+    udi.validate()
+    assert udi.accessible == "not a file"
+
+
+def test_permissions_file():
+    """Test building a UnstructuredData with no read permissions."""
+    udi = UnstructuredDataItem("file:///etc/sudoers", "text/plain")
+    udi.validate()
+    assert udi.accessible.lower().find("permission denied") != -1
+
+
+def test_invalid_url_scheme():
+    """Test building a UnstructuredData with invalid URL scheme."""
+    udi = UnstructuredDataItem("spam://example.com/", "text/plain")
+    udi.validate()
+    assert udi.accessible.lower().startswith("unknown url scheme")
 
 
 def test_invalid_mediatype():
