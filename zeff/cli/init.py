d85cb5f1f85fc76bea553984b02769fbb6dd8870
lanhel <lanhel@flyingtitans.com>
2019-08-10 17:34:59 -0600

    Refactor init

--- zeff/cli/init.py
+++ zeff/cli/init.py
@@ -25,144 +25,140 @@ def init_subparser(subparsers):
 
 def init_project(options):
     """Initialize a new project in the current directory."""
-
-    create_zeff_conf(options)
-    create_dataset(options)
-    create_generator(options)
-    create_builder(options)
+    Project(options)()
 
 
-def create_zeff_conf(options):
-    """Ask user for configuration options and create `zeff.conf`.
+class Project:
+    """Create a new project."""
 
-    This will create or update the zeff.conf file and will update the
-    configuration that is in options for use in other init operations.
-    """
-    optconf = options.configuration
-    config = ConfigParser(
-        strict=True,
-        allow_no_value=False,
-        delimiters=["="],
-        comment_prefixes=["#"],
-        interpolation=ExtendedInterpolation(),
-    )
-    config.read(CONF_PATH)
-    for section in (s for s in optconf.sections() if s not in config.sections()):
-        config.add_section(section)
-
-    def ask_update(section, option, msg, use_variables=False):
-        value = optconf.get(section, option, fallback="")
-        prompt = f"{msg} [{value}]? "
-        resp = input(prompt)
-        if resp and resp != value:
-            value = resp
-            if use_variables:
-                for defk, defv in optconf.defaults().items():
-                    scratch = resp.replace(defv, f"${{{defk.upper()}}}")
-                    if len(scratch) < len(value):
-                        value = scratch
-            config.set(section, option, value)
-        return value
-
-    # Server
-    ask_update("server", "server_url", "Server URL")
-    ask_update("server", "org_id", "Organization ID")
-    ask_update("server", "user_id", "User ID")
-
-    # Records
-    ask_update("records", "dataset_title", "Dataset Title")
-    ask_update("records", "dataset_desc", "Dataset Description")
-
-    ask_update(
-        "records", "records_config_generator", "Configuration generator python name"
-    )
-    ask_update(
-        "records",
-        "records_config_arg",
-        "Configuration generator init argument",
-        use_variables=True,
-    )
-    ask_update("records", "record_builder", "Record builder python name")
-    ask_update(
-        "records",
-        "record_builder_arg",
-        "Record builder init argument",
-        use_variables=True,
-    )
-
-    with open(CONF_PATH, "wt") as fout:
-        config.write(fout)
-
-    for defk, defv in optconf.defaults().items():
-        config.set("DEFAULT", defk, defv)
-
-    optconf.read_dict(config)
-
-
-def create_dataset(options):
-    """Create dataset on server and update config file."""
-    if options.configuration.get("records", "datasetid") == "":
-        config = ConfigParser(
+    def __init__(self, options):
+        self.options = options
+        self.optconf = self.options.configuration
+        self.config = ConfigParser(
             strict=True,
             allow_no_value=False,
             delimiters=["="],
             comment_prefixes=["#"],
             interpolation=ExtendedInterpolation(),
         )
-        config.read(CONF_PATH)
+        self.config.read(CONF_PATH)
+        for section in (
+            s for s in self.optconf.sections() if s not in self.config.sections()
+        ):
+            self.config.add_section(section)
 
-        try:
-            dataset_title = config.get("records", "dataset_title")
-            dataset_desc = config.get("records", "dataset_desc")
-        except NoOptionError:
-            return
+    def __call__(self):
+        self.create_zeff_conf()
+        self.create_dataset()
+        self.create_generator()
+        self.create_builder()
 
-        resource_map = ZeffCloudResourceMap(
-            ZeffCloudResourceMap.default_info(),
-            root=options.configuration.get("server", "server_url"),
-            org_id=options.configuration.get("server", "org_id"),
-            user_id=options.configuration.get("server", "user_id"),
-        )
-        dataset = Dataset.create_dataset(resource_map, dataset_title, dataset_desc)
-        datasetid = dataset.dataset_id
-
-        config.set("records", "datasetid", datasetid)
         with open(CONF_PATH, "wt") as fout:
-            config.write(fout)
+            self.config.write(fout)
 
+        for defk, defv in self.optconf.defaults().items():
+            self.config.set("DEFAULT", defk, defv)
 
-def create_generator(options):
-    """Create record generator template code."""
-    path = options.configuration["records"]["records_config_generator"]
-    create_python_from_template(path, "RecordGenerator", "RecordGenerator.template")
+        self.optconf.read_dict(self.config)
 
+    def create_zeff_conf(self):
+        """Ask user for configuration options and create `zeff.conf`.
 
-def create_builder(options):
-    """Create record builder template code."""
-    path = options.configuration["records"]["record_builder"]
-    create_python_from_template(path, "RecordBuilder", "RecordBuilder.template")
+        This will create or update the zeff.conf file and will update the
+        configuration that is in options for use in other init operations.
+        """
 
+        def ask_update(section, option, msg, use_variables=False):
+            value = self.optconf.get(section, option, fallback="")
+            prompt = f"{msg} [{value}]? "
+            resp = input(prompt)
+            if resp and resp != value:
+                value = resp
+                if use_variables:
+                    for defk, defv in self.optconf.defaults().items():
+                        scratch = resp.replace(defv, f"${{{defk.upper()}}}")
+                        if len(scratch) < len(value):
+                            value = scratch
+                self.config.set(section, option, value)
+            return value
 
-def create_python_from_template(path, name_ext, template_name):
-    """Create a source file from a template."""
-    m_name, c_name = path.rsplit(".", 1)
-    try:
-        module = importlib.import_module(m_name)
-        getattr(module, c_name)
-        return
-    except ModuleNotFoundError:
-        pass
-    except AttributeError:
-        pass
+        # Server
+        ask_update("server", "server_url", "Server URL")
+        ask_update("server", "org_id", "Organization ID")
+        ask_update("server", "user_id", "User ID")
 
-    name = c_name.replace(name_ext, "")
-    if name == "":
-        name = Path.cwd().name
+        # Records
+        ask_update("records", "dataset_title", "Dataset Title")
+        ask_update("records", "dataset_desc", "Dataset Description")
 
-    template_path = PurePath(__file__).parent.joinpath(template_name)
-    with open(template_path, "rt") as template_file:
-        template = Template(template_file.read())
+        ask_update(
+            "records", "records_config_generator", "Configuration generator python name"
+        )
+        ask_update(
+            "records",
+            "records_config_arg",
+            "Configuration generator init argument",
+            use_variables=True,
+        )
+        ask_update("records", "record_builder", "Record builder python name")
+        ask_update(
+            "records",
+            "record_builder_arg",
+            "Record builder init argument",
+            use_variables=True,
+        )
 
-    output_path = Path.cwd() / f"{m_name}.py"
-    with open(output_path, "wt") as fout:
-        fout.write(template.safe_substitute(name=name, c_name=c_name))
+    def create_dataset(self):
+        """Create dataset on server and update config file."""
+        if self.options.configuration.get("records", "datasetid") == "":
+            try:
+                dataset_title = self.config.get("records", "dataset_title")
+                dataset_desc = self.config.get("records", "dataset_desc")
+            except NoOptionError:
+                return
+            resource_map = ZeffCloudResourceMap(
+                ZeffCloudResourceMap.default_info(),
+                root=self.optconf.get("server", "server_url"),
+                org_id=self.optconf.get("server", "org_id"),
+                user_id=self.optconf.get("server", "user_id"),
+            )
+            dataset = Dataset.create_dataset(resource_map, dataset_title, dataset_desc)
+            datasetid = dataset.dataset_id
+            self.config.set("records", "datasetid", datasetid)
+
+    def create_generator(self):
+        """Create record generator template code."""
+        self.create_python_from_template(
+            "records_config_generator", "RecordGenerator", "RecordGenerator.template"
+        )
+
+    def create_builder(self):
+        """Create record builder template code."""
+        self.create_python_from_template(
+            "record_builder", "RecordBuilder", "RecordBuilder.template"
+        )
+
+    def create_python_from_template(self, option, name_ext, template_name):
+        """Create a source file from a template."""
+        path = self.optconf["records"][option]
+        m_name, c_name = path.rsplit(".", 1)
+        try:
+            module = importlib.import_module(m_name)
+            getattr(module, c_name)
+            return
+        except ModuleNotFoundError:
+            pass
+        except AttributeError:
+            pass
+
+        name = c_name.replace(name_ext, "")
+        if name == "":
+            name = Path.cwd().name
+
+        template_path = PurePath(__file__).parent.joinpath(template_name)
+        with open(template_path, "rt") as template_file:
+            template = Template(template_file.read())
+
+        output_path = Path.cwd() / f"{m_name}.py"
+        with open(output_path, "wt") as fout:
+            fout.write(template.safe_substitute(name=name, c_name=c_name))
