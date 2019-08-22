bdd16f924c8107ca325b409b220af9bf6750fb17
lanhel <lanhel@flyingtitans.com>
2019-08-22 10:30:25 -0600

    Train status
    fixes #15
    
    Training status can be displayed one time or display a progress bar.

--- zeff/cli/train.py
+++ zeff/cli/train.py
@@ -2,9 +2,14 @@
 __docformat__ = "reStructuredText en"
 __all__ = ["train_subparser"]
 
+import sys
+import errno
 import zeff
-import zeff.record
 from .server import subparser_server
+import zeff.record
+from zeff.zeffcloud import ZeffCloudResourceMap
+from zeff.cloud.exception import ZeffCloudException
+from zeff.cloud.dataset import Dataset, TrainingSessionInfo
 
 
 def train_subparser(subparsers, config):
@@ -15,23 +20,105 @@ def train_subparser(subparsers, config):
 
     parser = subparsers.add_parser("train", help="""Control training sessions.""")
     parser.add_argument(
-        "action",
-        choices=["start", "stop", "kill"],
-        help="""Perform the given action on Zeff Cloud:
-            start - start training the current session,
-            stop - stop training the current session,
-            kill - kill current session and mark as invalid""",
+        "--records-datasetid",
+        default=config["records"]["datasetid"],
+        help="""Dataset id to access for training.""",
     )
     subparser_server(parser, config)
     parser.set_defaults(func=train)
 
+    actions = parser.add_subparsers(
+        help="Send commands for training sessions on the specified dataset."
+    )
+
+    action_status = actions.add_parser(
+        "status", help="Display status of current training session."
+    )
+    action_status.add_argument(
+        "--continuous",
+        default=False,
+        action="store_true",
+        help="Continuously display the status of the training session.",
+    )
+    action_status.set_defaults(action=Trainer.status)
+
+    action_start = actions.add_parser(
+        "start", help="Start or restart a training session."
+    )
+    action_start.set_defaults(action=Trainer.start)
+
+    action_stop = actions.add_parser("stop", help="Stop the current training session.")
+    action_stop.set_defaults(action=Trainer.stop)
+
+    action_kill = actions.add_parser("kill", help="Kill the current training session.")
+    action_kill.set_defaults(action=Trainer.kill)
+
 
 def train(options):
     """Generate a set of records from options."""
-    trainer = zeff.Trainer()
-    if options.action == "start":
-        trainer.start()
-    elif options.action == "stop":
-        trainer.stop()
-    elif options.action == "kill":
-        trainer.kill()
+    if not options.records_datasetid:
+        print("Unknown dataset id to access for training.", file=sys.stderr)
+        sys.exit(errno.EINVAL)
+    trainer = Trainer(options)
+    options.action(trainer)
+
+
+class Trainer:
+    """Controller for dataset training."""
+
+    def __init__(self, options):
+        """Create new trainier controller."""
+        self.options = options
+        self.server_url = options.server_url
+        self.org_id = options.org_id
+        self.user_id = options.user_id
+        self.dataset_id = options.records_datasetid
+
+        info = ZeffCloudResourceMap.default_info()
+        self.resource_map = ZeffCloudResourceMap(
+            info, root=self.server_url, org_id=self.org_id, user_id=self.user_id
+        )
+        self.dataset = Dataset(self.dataset_id, self.resource_map)
+
+    def status(self):
+        """Print current status to stream."""
+        from tqdm import tqdm
+        import time
+
+        if self.options.continuous:
+            s = self.dataset.training_status
+            pbar = tqdm(
+                desc=str(s.status),
+                total=100,
+                bar_format="{l_bar}{bar}| {n_fmt}/{total_fmt} [{elapsed}]",
+            )
+            while s.status is not TrainingSessionInfo.Status.complete:
+                pbar.set_description(str(s.status))
+                pbar.update(s.progress)
+                time.sleep(1.0)
+                s = self.dataset.training_status
+            pbar.close()
+        else:
+            s = self.dataset.training_status
+            if s.status is TrainingSessionInfo.Status.queued:
+                print(f"Queued as of {s.updated_timestamp.strftime('%c')}")
+            elif s.status is TrainingSessionInfo.Status.started:
+                print(f"Started on {s.updated_timestamp.strftime('%c')}")
+            elif s.status is TrainingSessionInfo.Status.progress:
+                print(
+                    f"Progress {s.progress:.2%} as of {s.updated_timestamp.strftime('%c')}"
+                )
+            elif s.status is TrainingSessionInfo.Status.complete:
+                print(f"Completed on {s.updated_timestamp.strftime('%c')}")
+
+    def start(self):
+        """Start or restart the current training session."""
+        self.dataset.start_training()
+
+    def stop(self):
+        """Stop the current training session."""
+        self.dataset.stop_training()
+
+    def kill(self):
+        """Kill the current training session and mark invalid."""
+        self.dataset.kill_training()
--- zeff/cloud/dataset.py
+++ zeff/cloud/dataset.py
@@ -2,7 +2,10 @@
 __docformat__ = "reStructuredText en"
 
 import logging
+import enum
 import json
+import datetime
+import collections
 from typing import Iterator
 from .exception import ZeffCloudException
 from .resource import Resource
@@ -127,3 +130,98 @@ class Dataset(Resource):
             data["location"],
         )
         return Record(self, data["recordId"])
+
+    @property
+    def training_status(self):
+        """Return current training status metrics object."""
+        tag = "tag:zeff.com,2019-07:datasets/train"
+        resp = self.request(tag, method="GET", dataset_id=self.dataset_id)
+        if resp.status_code not in [200]:
+            raise ZeffCloudException(
+                resp, type(self), self.dataset_id, "training status"
+            )
+        return TrainingSessionInfo(resp.json()["data"])
+
+    def start_training(self):
+        """Start or restart the current training session."""
+        tag = "tag:zeff.com,2019-07:datasets/train"
+        resp = self.request(tag, method="PUT", dataset_id=self.dataset_id)
+        if resp.status_code not in [202]:
+            raise ZeffCloudException(
+                resp, type(self), self.dataset_id, "training start"
+            )
+
+    def stop_training(self):
+        """Stop the current training session."""
+        # pylint: disable=no-self-use
+        logging.debug("Stop de training")
+
+    def kill_training(self):
+        """Kill the current training session and mark invalid."""
+        # pylint: disable=no-self-use
+        logging.debug("Kill the training session")
+
+
+class TrainingSessionInfo:
+    """Information about the current training session."""
+
+    class Status(enum.Enum):
+        """Training status of training session."""
+
+        unknown = "UNKNOWN"
+        queued = "QUEUED"
+        started = "STARTED"
+        progress = "PCT_COMPLETE"
+        complete = "COMPLETE"
+
+        def __str__(self):
+            return self.name
+
+        def __repr__(self):
+            return "<%s.%s>" % (self.__class__.__name__, self.name)
+
+    def __init__(self, status_json):
+        """Create a new training information.
+
+        :param status_json: The status JSON returned from a train
+            status request.
+        """
+        self.__data = status_json
+
+    @property
+    def status(self) -> "TrainingSessionInfo.Status":
+        r = self.__data["status"]
+        return TrainingSessionInfo.Status(r if r is not None else "unknown")
+
+    @property
+    def progress(self) -> float:
+        r = self.__data["percentComplete"]
+        return float(r) if r is not None else 0.0
+
+    @property
+    def model_version(self) -> str:
+        r = self.__data["modelVersion"]
+        return r if r is not None else "unknown"
+
+    @property
+    def model_location(self) -> str:
+        r = self.__data["modelLocation"]
+        return r if r is not None else "unknown"
+
+    @property
+    def created_timestamp(self) -> datetime.datetime:
+        r = self.__data["createdAt"]
+        if r is not None:
+            d = datetime.datetime.fromisoformat(r)
+            return d
+        else:
+            return None
+
+    @property
+    def updated_timestamp(self) -> datetime.datetime:
+        r = self.__data["updatedAt"]
+        if r is not None:
+            d = datetime.datetime.fromisoformat(r)
+            return d
+        else:
+            return self.created_timestamp
